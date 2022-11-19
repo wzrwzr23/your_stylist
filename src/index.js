1d5d3af76fb3991e91326c8575dc4b2c3249a577
yuehengggg <97967305+yuehengggg@users.noreply.github.com>
2022-11-19 14:31:39 +0800

    1

--- src/index.js
+++ src/index.js
@@ -15,3 +15,23 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const { MongoClient } = require("mongodb");
+// Replace the uri string with your connection string.
+const uri =
+"mongodb+srv://yuehengggg:1004866wyh1103@cluster0.5keddjz.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
