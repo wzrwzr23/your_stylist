58897ec4f7d6a9563374a85bb9493c53d4414012
yuehengggg <97967305+yuehengggg@users.noreply.github.com>
2022-11-19 16:03:25 +0800

    database connect

--- node_connect/index.js
+++ node_connect/index.js
@@ -1,19 +1,9 @@
-const MongoClient = require("mongodb").MongoClient;
-// Replace the uri string with your connection string.
-const uri =
-"mongodb+srv://yuehengggg:1004866wyh1103@cluster0.5keddjz.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://yuehengggg:1004866wyh1103A@stylist.59vtveq.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
