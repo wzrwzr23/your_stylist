d3f851b7b50ba60c22d3d0da91266efa78ccfdc1
yuehengggg <97967305+yuehengggg@users.noreply.github.com>
2022-11-21 23:16:00 +0800

    current change

--- server/routes/Men.js
+++ server/routes/Men.js
@@ -1,25 +1,58 @@
 require("../db/config");
-const express = require("express");
+const mongoose = require("mongoose");
+
 const Men = require("../db/Men");
+const express = require("express");
+//const Men =mongoose.model("Blazers");
 
 const router = express.Router();
 
-router.post("/", async (re, rs) => {
-  let rt = new Men(re.body);
-  let rl = await rt.save();
-  rs.send(rl);
-});
 
-router.get("/", (req, res) => {
-    Men.find({}, (err, result) => {
-        if (err) console.log(err);
-        else {
-            console.log("Getting Information...")
-            res.json(result)
-        }
-    })
-})
 
+// router.post("/", async (re, rs) => {
+//   let rt = new Men(re.body);
+//   let rl = await rt.save();
+//   rs.send(rl);
+// });
+
+// router.get("/", (req, res) => {
+//     Men.find({}, (err, result) => {
+//         if (err) console.log(err);
+//         else {
+//             console.log("Getting Information...")
+//             res.json(result)
+//         }
+//     })
+// })
+
+router.get("/", async (req, res) => {
+    const query = req.query.new;
+    try {
+      const men = query
+        ? await Men.find().sort({ _id: -1 }).limit(5)
+        : await Men.find();
+      res.status(200).json(men);
+      res.status(200).send(men);
+      console.log(men);
+    } catch (err) {
+      res.status(500).json(err);
+      res.status(500).send(err);
+      console.log(err);
+    }
+  });
+
+  router.post("/", async (req, res) => {
+    const newMen = new Men(req.body);
+  
+    try {
+      const saveMen = await newMen.save();
+      res.status(200).json(saveMen);
+      res.send(saveMen);
+    } catch (err) {
+      res.status(500).json(err);
+      res.send(err);
+    }
+  });
  
 //app.use(router);
 
--- server/routes/Women.js
+++ server/routes/Women.js
@@ -1,45 +1,68 @@
-require("../db/config");
+//const Women= 
+//require("../db/config");
 const express = require("express");
-const Women = require("../db/Women");
+//const { default: mongoose } = require("mongoose");
+//const WomenModel = require("../db/Women");
+//const { db } = require("../db/Men");
+//const Women = require("../db/Women"); 
+//const Women = require("../db/Women");
+//const Women=mongoose.model(Women).collection
+//const config = require("../db/config");
+//const db=config.
+
+//const Women = mongoose.connect("mongodb://yuehengggg:1004866wyh1103A@ac-dtwqfjg-shard-00-00.59vtveq.mongodb.net:27017,ac-dtwqfjg-shard-00-01.59vtveq.mongodb.net:27017,ac-dtwqfjg-shard-00-02.59vtveq.mongodb.net:27017/?ssl=true&replicaSet=atlas-otyugf-shard-0&authSource=admin&retryWrites=true&w=majority")
+//const Women =mongoose.connection.db('Women')
+
+//const db = require("../db/config");s
 
 const router = express.Router();
 
-//const app = express();
-
-// app.get("/", (req, res) => {
-//     Women.find({}, (err, result) => {
-//         if (err) console.log(err);
-//         else {
-//             console.log("Getting Information...")
-//             // res.json(result)
-//             res.send(result)
-//         }
-//     })
-// })
-
-router.post("/", async (re, rs) => {
-  let rt = new Women(re.body);
-  let rl = await rt.save();
-  rs.send(rl);
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://yuehengggg:1004866wyh1103A@stylist.59vtveq.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const dbo = client.db('Women')
+    const collection = client.db("Women").collection("Blouse");
+  // perform actions on the collection object
+    console.log("Connected to database");
+    router.post("/", async (re, rs) => {
+        let rt = new collection(re.body);//
+        let rl = await rt.save();
+        rs.send(rl);
+    });
+    
+    router.get("/", (req, res) => {
+        // collection.find({}, function (err, docs) {
+        //     console.log(docs)
+        //     res.send(docs)
+        //    });
+        // dbo.collection("Blouse").find({}, (err, docs) =>{
+        //     console.log(docs)
+        //     res.send(docs)
+        //    });
+            dbo.collection("Blouse").find({}).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);})
+            // res.send(result)
+       // res.send("1");
+    })
+    module.exports = router;
+    
+  client.close();
 });
 
-router.get("/", (req, res) => {
-    Women.find({}, (err, result) => {
-        if (err) console.log(err);
-        else {
-            console.log("Getting Information...")
-            res.json(result)
-            res.send(result)
-        }
-    })
-})
+// router.post("/", async (re, rs) => {
+//   let rt = new Women(re.body);//
+//   let rl = await rt.save();
+//   rs.send(rl);
+// });
+
+// router.get("/", (req, res) => {
+//     Women.find({}, function (err, docs) {
+//         console.log(docs)
+//         res.send(docs)
+//        });
+// })
 
  
-// app.listen(8000, () => {
-//     console.log("Server Woman is listening");
-//   });
 
-
-
-
- module.exports = router;
